ba47403b2b200eb07b12b83a809931905b742f46
wiechab <wiechab@outlook.com>
2017-09-10 11:10:29 +0200

    new project

---
+++ src/pl/atena/edu/akademia00/StartZero.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pl.atena.edu.akademia00;
+
+/**
+ * @author wieslawab
+ *
+ */
+public class StartZero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Start One");
+	}
+
+}
