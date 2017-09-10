eeb2b4f8fc4dc0fbde18eb3fa076518c100be6f0
wiechab <wiechab@outlook.com>
2017-09-10 16:57:30 +0200

    c.d.

--- src/pl/atena/edu/akademia00/Pole.java
+++ src/pl/atena/edu/akademia00/Pole.java
@@ -9,40 +9,65 @@ import java.util.Scanner;
  * @author wieslawab
  *
  */
-public class Pole {
+public class Pole{
 
 	/**
 	 * @param args
 	 */
-	
+
 	private static Scanner scanner = new Scanner(System.in);
-	
+	public static final int MIN = 1;
+	public static final int MAX = 10;
+
+	/**
+	 * @param liczba
+	 * @param komunikat
+	 * @return
+	 */
+	public static boolean sprawdz(int liczba, String komunikat) {
+		if (!(liczba >= MIN && liczba <= MAX)) {
+			System.out.println(komunikat);
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Podaj wartoœæ boku a:  ");
 		String sa = scanner.nextLine();
-		
+
 		System.out.println("Podaj wartoœæ boku b:  ");
 		String sb = scanner.nextLine();
-		
+
 		int ia = Integer.valueOf(sa);
 		int ib = Integer.valueOf(sb);
-		System.out.println(ia*ib);
-		
+
+		boolean testA = sprawdz(ia, "Liczba a poza zakresem od " + MIN + " do " + MAX);
+		boolean testB = sprawdz(ib, "Liczba b poza zakresem od " + MIN + " do " + MAX);
+
+		if (!testA || !testB) {
+			return;
+		}
+
+		System.out.println("Pole prostok¹ta = " + (ia * ib));
+
+		if (ia > ib) {
+			System.out.println("Bok a szerszy od b pole: " + (ia * ib));
+		} else if (ia == ib) {
+			System.out.println("To jest kwadrat o polu: " + (ia * ib));
+		}
+
 		/*
-		
-		int bokA = 10;
-		int bokB = 2;
-		
-		int pole = Math.multiplyExact(bokA,bokB);
-		System.out.println(pole);
-		
-		
-		 bokA = 10;
-		 bokB = 2;
-		System.out.println(bokA * bokB);
-		*/
+		 * 
+		 * int bokA = 10; int bokB = 2;
+		 * 
+		 * int pole = Math.multiplyExact(bokA,bokB); System.out.println(pole);
+		 * 
+		 * 
+		 * bokA = 10; bokB = 2; System.out.println(bokA * bokB);
+		 */
 	}
 
 }
--- src/pl/atena/edu/akademia00/Stringi.java
+++ src/pl/atena/edu/akademia00/Stringi.java
@@ -16,32 +16,114 @@ public class Stringi {
 	 */
 	private static Scanner scanner = new Scanner(System.in);
 	
+	public static final int MIN = 1;
+	public static final int MAX = 12;
+	
+	public static String podajMiesiac(int liczba) {
+		String miesiac = null;
+		
+		if (!(liczba >= MIN && liczba <= MAX)) {
+			System.out.println("Poda³eœ b³êdny miesi¹c !!!");
+			
+			
+			return "B£¥D";
+		}
+		
+		
+		switch (liczba) {
+		case 1:
+			miesiac = "styczeñ";
+			break;
+		case 2:
+			miesiac = "luty";
+			break;
+		case 3:
+			miesiac = "marzec";
+			break;
+		case 4:
+			miesiac = "kwiecieñ";
+			break;
+		case 5:
+			miesiac = "maj";
+			break;
+		case 6:
+			miesiac = "czerwiec";
+			break;
+		case 7:
+			miesiac = "lipiec";
+			break;
+		case 8:
+			miesiac = "sierpieñ";
+			break;
+			
+		case 9:
+			miesiac = "wrzesieñ";
+			break;
+			
+		case 10:
+			miesiac = "pa¿dziernik";
+			break;
+			
+		case 11:
+			miesiac = "listopad";
+			break;
+			
+		case 12:
+			miesiac = "grudzieñ";
+			
+			break;
+			default:
+				break;
+		
+		}
+	
+		return miesiac;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Podaj ci¹g znaków 1:  ");
 		String tekst1 = scanner.nextLine();
-		
-		
-		System.out.println("Podaj ci¹g znaków 1::  ");
+
+		System.out.println("Podaj ci¹g znaków 2:  ");
 		String tekst2 = scanner.nextLine();
 		
-		System.out.println("D³ugoœæ tekstu1: " + tekst1.length() +"; D³ugoœæ tekstu2: " + tekst2.length());
+		System.out.println("Podaj miesi¹c:  ");
+		String tekst3 = scanner.nextLine();
 		
-		//UWAGA!!! Dla obiektu String NIE STOSUJEMY PORÓWNANIA TYPU ==
-		//tekst1 == tekst2 ¯LE
+		int mc = Integer.valueOf(tekst3);
 		
-		if (tekst1.equals(tekst2) ) {
-			System.out.println("Teksty identyczne");
-			
-			
+		String nazwaMc = podajMiesiac(mc);
+		
+		if(! (nazwaMc.equals("B£¥D"))) {
+			System.out.println("Poda³eœ miesi¹c : " + nazwaMc);
 		}
-		else
-		{
-			System.out.println("Teksty ró¿ne");	
+
+
+		System.out.println("D³ugoœæ tekstu1: " + tekst1.length() + "; D³ugoœæ tekstu2: " + tekst2.length());
+
+		// UWAGA!!! Dla clasy String NIE STOSUJEMY PORÓWNANIA TYPU ==
+		// tekst1 == tekst2 ¯LE dobrze jest tak tekst1.equals(tekst2)
+
+		if (tekst1.equals(tekst2)) {
+			System.out.println("Teksty s¹ identyczne");
+
+		} else {
+			System.out.println("Teksty s¹ ró¿ne");
+		}
+
+		String ciag = String.join(", ", "Jurek", "Marek", "Wacek");
+		System.out.println(ciag);
+		
+		String[] tablica = ciag.split(",");
+		
+		for (int i = 0 ; i < tablica.length ; i++) {
+			
+			tablica[i] = (tablica[i].trim());
+			System.out.println(tablica[i]);
 		}
 		
-			String ciag = String.join(", ", "Jurek","Marek","Wacek");
-			System.out.println(ciag);
+
 	}
 
 }
